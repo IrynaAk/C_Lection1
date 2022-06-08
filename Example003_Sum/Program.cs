9689ba2876c4924a539e93c83658f4cb0314d5a1
Iryna_Akunevich <akunevichirina@mail.ru>
2022-06-08 23:26:29 +0300

    Sum of rendom numbers

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,13 @@
+﻿//Console.WriteLine("Type the first number");
+//int number1 = Console.ReadLine();
+//Console.WriteLine("Type the second number");
+//int number2 = Console.ReadLine();
+//int result = number1 + number2;
+//Console.WriteLine(result);
+
+int number1 = new Random().Next(2, 14);
+int number2 = new Random().Next(6, 17);
+int result = number1 + number2;
+Console.WriteLine(number1);
+Console.WriteLine(number2);
+Console.WriteLine(result);
