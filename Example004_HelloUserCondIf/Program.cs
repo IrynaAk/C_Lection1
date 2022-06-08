557c7b06877776abd85ace7407549438365be464
Iryna_Akunevich <akunevichirina@mail.ru>
2022-06-08 23:50:41 +0300

    Hello User with condition

---
+++ Example004_HelloUserCondIf/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Please, type your name");
+string name = Console.ReadLine();
+
+if (name.ToLower() == "iryna")
+{
+    Console.Write("Hello, my dear Iryna!");
+    } 
+    else 
+    {
+    Console.Write("Hi, ");
+    Console.WriteLine(name);
+}
